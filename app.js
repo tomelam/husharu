b2ba8fea4b59a68ca4c0a71d98bd608930c36ca8
Ritesh Kadmawala <k.g.ritesh@gmail.com>
2011-09-24 21:53:57 +0530

    First attempt at getting twitter and facebook connect

--- app.js
+++ app.js
@@ -4,37 +4,81 @@
  */
 
 require.paths.push('/usr/local/lib/node_modules');
-var express = require('express');
+var express = require('express'),
+    everyauth = require('everyauth'),
+    conf = require('./conf'),
+    app = module.exports = express.createServer();
 
-var app = module.exports = express.createServer();
-
+everyauth.debug = true;
 // Configuration
+
+
+var usersById = {};
+var nextUserId = 0;
+var usersByFbId = {};
+var usersByTwitId = {};
+
+function addUser (source, sourceUser) {
+  var user;
+  if (arguments.length === 1) { // password-based
+    user = sourceUser = source;
+    user.id = ++nextUserId;
+    return usersById[nextUserId] = user;
+  } else { // non-password-based
+    user = usersById[++nextUserId] = {id: nextUserId};
+    user[source] = sourceUser;
+  }
+  return user;
+}
+
+
+everyauth
+  .facebook
+    .appId(conf.fb.appId)
+    .appSecret(conf.fb.appSecret)
+    .findOrCreateUser( function (session, accessToken, accessTokenExtra, fbUserMetadata) {
+        return usersByFbId[fbUserMetadata.id] ||
+            (usersByFbId[fbUserMetadata.id] = addUser('facebook', fbUserMetadata));
+    })
+    .redirectPath('/');
+
+everyauth
+  .twitter
+    .consumerKey(conf.twit.consumerKey)
+    .consumerSecret(conf.twit.consumerSecret)
+    .findOrCreateUser( function (sess, accessToken, accessSecret, twitUser) {
+      return usersByTwitId[twitUser.id] || (usersByTwitId[twitUser.id] = addUser('twitter', twitUser));
+    })
+    .redirectPath('/');
+
+
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'htuayreve'}));
+  app.use(everyauth.middleware());
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+  app.use(express.errorHandler());
 });
 
 // Routes
 
 app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
-  });
+    res.render('home')
 });
 
+everyauth.helpExpress(app);
+
 app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
---
+++ conf.js
@@ -0,0 +1,11 @@
+module.exports = {
+    fb: {
+        appId: '172654286146716',
+        appSecret:  'dd86137a82acd90475b60aa0e77fc365'
+    },
+    twit: {
+        consumerKey: 'Y8JxpjlkI9wg7gHHmN1k6g',
+        consumerSecret: 'N0Nwyov7xVedCVmdp6xmvX1AwE9wmrNBWK6ufbl5xKI'
+    }
+
+};
